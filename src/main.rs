47c442c57f9681e4bebe98aacb2b0c3ad788820d
Denis Kokin <denis.kokin@gmail.com>
2024-07-03 10:45:09 +0200

    Implementing simple blinky
    Example taken from https://github.com/Rahix/avr-hal/blob/main/examples/trinket/src/bin/trinket-blink.rs

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,18 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
+    // Digital pin 1 is also connected to an onboard LED marked "L"
     let mut led = pins.d1.into_output();
+    led.set_high();
 
     loop {
         led.toggle();
-        arduino_hal::delay_ms(1000);
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(800);
     }
 }
